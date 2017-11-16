09a99e58fde23a47e35dfa7af69669503578b161
Mike Murray <michaelbrazell@gmail.com>
2017-11-16 17:05:03 -0500

    Setup babel as a project dependency

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
+'use strict';
+
 console.log('App.js is running');
 
 // JSX 
-var template = <p>This is JSX from app.js</p>
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Indecision App
+## Just a learning project of React & Redux
+
+#### Commands to run:
+For watching babel changes: `$ babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch`
+For dependency management with yarn: `$ yarn install`
