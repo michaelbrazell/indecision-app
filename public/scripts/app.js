5d638875437dee446274220cb53b86f0d5476be5
Mike Murray <michaelbrazell@gmail.com>
2018-01-09 22:04:39 -0500

    Bind data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -30,40 +30,47 @@ var template = React.createElement(
 
 var count = 0;
 var addOne = function addOne() {
-  console.log('Add One!');
+  count++;
+  renderCounterApp();
 };
 var minusOne = function minusOne() {
-  console.log('Minus One!');
+  count--;
+  renderCounterApp();
 };
 var reset = function reset() {
-  console.log('Reset!');
+  count = 0;
+  renderCounterApp();
 };
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { onClick: addOne },
-    '+1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: minusOne },
-    '-1'
-  ),
-  React.createElement(
-    'button',
-    { onClick: reset },
-    'Reset'
-  )
-);
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
